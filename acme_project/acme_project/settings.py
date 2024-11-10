f487403192a9ffb83ed6158fb571dbead3325fa1
Dmitry Kudrin <super.kda5116@yandex.ru>
2024-11-10 21:34:37 +0300

    v_0.9

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -6,11 +6,19 @@ SECRET_KEY = 'django-insecure-m&$lzdzkutvrbr5vt=jpm)7#g7cken_tk%($ty+w902n7wb#=e
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    # Когда проект будет опубликован и станет доступен для пользователей,
+    # в этот список нужно будет добавить и адреса домена, где он будет размещён,
+    # например 'acme.not' и 'www.acme.not'
+] 
+
+CSRF_FAILURE_VIEW = 'core.views.csrf_failure'
 
 INSTALLED_APPS = [
     'django.contrib.admin',
-    'django.contrib.auth',
+    'django.contrib.auth',    # Приложение для работы с пользователями.
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -89,3 +97,12 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = BASE_DIR / 'media' 
+
+# Подключаем бэкенд filebased.EmailBackend:
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+# Указываем директорию, в которую будут сохраняться файлы писем:
+EMAIL_FILE_PATH = BASE_DIR / 'sent_emails'
+
+LOGIN_REDIRECT_URL = 'pages:homepage' 
+
+LOGIN_URL = 'login'
--- acme_project/birthday/admin.py
+++ acme_project/birthday/admin.py
@@ -1,1 +1,22 @@
 from django.contrib import admin
+
+from .models import Tag
+
+admin.site.register(Tag,)
+
+# class TagAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'title',
+#         'description',
+#         'is_published',
+#         'slug',
+#     )
+#     list_editable = (
+#         'is_published',
+#         'slug',
+#     )
+#     # search_fields = ('title', 'slug',)
+#     # list_filter = ('title', 'slug',)
+#     list_display_links = ('title',)
+
+# admin.site.register(Tag,)
